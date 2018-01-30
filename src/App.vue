7c02773b32760f7ffa58cb2a0d11f69dbd2e7268
DenisShkain <denis.d.shakin@gmail.com>
2018-01-30 19:21:04 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <p>Motion</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  @import "assets/scss/index.scss";
+</style>
