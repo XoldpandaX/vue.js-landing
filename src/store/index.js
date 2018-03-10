e27815a7f8f4a112bfabc47a27f3c5e87b01ff7a
DenisShkain <denis.d.shakin@gmail.com>
2018-03-10 17:19:00 +0700

    added click-outside directive

--- src/store/index.js
+++ src/store/index.js
@@ -5,26 +5,6 @@ import modules from './modules';
 
 Vue.use(Vuex);
 
-Vue.directive('click-outside', {
-  priority: 700,
-  bind () {
-    let self  = this
-    this.event = function (event) {
-    	console.log('emitting event')
-    	self.vm.$emit(self.expression,event)
- 	  }
-    this.el.addEventListener('click', this.stopProp)
-    document.body.addEventListener('click',this.event)
-  },
-
-  unbind() {
-  	console.log('unbind')
-    this.el.removeEventListener('click', this.stopProp)
-    document.body.removeEventListener('click',this.event)
-  },
-  stopProp(event) {event.stopPropagation() }
-})
-
 export const store = new Vuex.Store({
   modules
 });
