ad9870bc0e4d786f70fc1985cf5163db6d73634c
DenisShkain <denis.d.shakin@gmail.com>
2018-03-11 17:23:55 +0700

    deleted mobile components | cleaned the code

--- src/blocks/HeaderBlock.vue
+++ src/blocks/HeaderBlock.vue
@@ -7,15 +7,11 @@
 </template>
 
 <script>
-  import { mapGetters } from 'vuex';
   import Logo from './../components/header/Logo';
   import StaticHamburgerIcon from './../components/icons/StaticHamburgerIcon';
   import Navigation from './../components/header/Navigation';
 
   export default {
-    computed: {
-      ...mapGetters('ui', ['mobileMenu'])
-    },
     components: {
       Logo,
       StaticHamburgerIcon,
