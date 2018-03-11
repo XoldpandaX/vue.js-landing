0eeb7893bb8c5a756d359d3f112cb6bc43824caf
DenisShkain <denis.d.shakin@gmail.com>
2018-03-11 12:21:54 +0700

    added new component mobile navigation

--- src/blocks/HeaderBlock.vue
+++ src/blocks/HeaderBlock.vue
@@ -4,18 +4,21 @@
       static-hamburger-icon
       logo
       navigation
+      mobile-navigation
 </template>
 
 <script>
   import Logo from './../components/header/Logo';
   import StaticHamburgerIcon from './../components/icons/StaticHamburgerIcon';
   import Navigation from './../components/header/Navigation';
+  import MobileNavigation from './../components/header/MobileNavigation';
 
   export default {
     components: {
       Logo,
       StaticHamburgerIcon,
-      Navigation
+      Navigation,
+      MobileNavigation
     }
   }
 </script>
