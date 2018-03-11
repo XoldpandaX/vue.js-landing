16875d4e6b86358ec7b33f917958d3b78186e291
DenisShkain <denis.d.shakin@gmail.com>
2018-03-11 12:15:22 +0700

    completed burger static icon | right media queries

--- src/blocks/HeaderBlock.vue
+++ src/blocks/HeaderBlock.vue
@@ -1,17 +1,20 @@
 <template lang="pug">
   div.page-header
     div.page-header__inner
+      static-hamburger-icon
       logo
       navigation
 </template>
 
 <script>
-  import Logo from './../components/header/Logo.vue';
-  import Navigation from './../components/header/Navigation.vue';
+  import Logo from './../components/header/Logo';
+  import StaticHamburgerIcon from './../components/icons/StaticHamburgerIcon';
+  import Navigation from './../components/header/Navigation';
 
   export default {
     components: {
       Logo,
+      StaticHamburgerIcon,
       Navigation
     }
   }
