a23274c68e09c6912c7c4454fcecf1ed55d556c5
DenisShkain <denis.d.shakin@gmail.com>
2018-03-11 14:54:52 +0700

    added media

--- src/components/header/MobileNavigation.vue
+++ src/components/header/MobileNavigation.vue
@@ -2,10 +2,12 @@
   .mobile-menu(:class="toggleMenu")
     button.mobile-menu__close-btn(@click="closeMobileMenu")
       cross-icon
+
 </template>
 
 <script>
   import { mapGetters, mapActions } from 'vuex';
+  import Navigation from './Navigation'
   import CrossIcon from './../icons/CrossIcon';
 
   export default {
