9a9a5f4799e57216d05aca19a982d9c0fd7bbdac
DenisShkain <denis.d.shakin@gmail.com>
2018-04-01 18:09:00 +0700

    added colors for slider && added data to slider-content

--- src/components/landing-components/slider/slider-content.js
+++ src/components/landing-components/slider/slider-content.js
@@ -8,5 +8,35 @@ export const sliderContent = [
       middleText: 'Material Design',
       bottomText: 'Vue.js landing page template'
     }
+  },
+  {
+    image: './src/assets/media/images/slider/guy.png',
+    color: '#e91e63',
+    buttonText: 'get started',
+    text: {
+      topText: 'Made by',
+      middleText: 'Denis Shakin',
+      bottomText: 'Especially for him'
+    }
+  },
+  {
+    image: './src/assets/media/images/slider/guy.png',
+    color: '#3949ab',
+    buttonText: 'get started',
+    text: {
+      topText: 'Based on the ideology',
+      middleText: 'ideology',
+      bottomText: 'of component approach'
+    }
+  },
+  {
+    image: './src/assets/media/images/slider/guy.png',
+    color: '#8e24aa',
+    buttonText: 'get started',
+    text: {
+      topText: 'Feel Power Of The',
+      middleText: 'Vue.js',
+      bottomText: 'and become stronger'
+    }
   }
 ];
