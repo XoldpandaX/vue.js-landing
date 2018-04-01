8292e74fe956c57dab2c30b14a9bd19de7e25a16
DenisShkain <denis.d.shakin@gmail.com>
2018-04-01 14:35:23 +0700

    added text content to slider

--- src/components/landing-components/slider/Slider.vue
+++ src/components/landing-components/slider/Slider.vue
@@ -3,10 +3,21 @@
     .slider__inner
       .slider__slide
         .slider__media
+        .slider__text-info
+          h2 {{ sliderContent[0].text.topText }}
+          h1 {{ sliderContent[0].text.middleText }}
+          h5 {{ sliderContent[0].text.bottomText }}
 </template>
 
 <script>
+  import {sliderContent} from "./slider-content.js";
+
   export default {
-    name: "slider"
+    name: "slider",
+    data() {
+      return {
+        sliderContent: sliderContent
+      }
+    }
   }
 </script>
---
+++ src/components/landing-components/slider/slider-content.js
@@ -0,0 +1,11 @@
+export const sliderContent = [
+  {
+    image: './assets/media/images/slider/guy.png',
+    backgroundColor: '#673ab7',
+    text: {
+      topText: 'This is the',
+      middleText: 'Material Design',
+      bottomText: 'Vue.js landing page template'
+    }
+  }
+];
