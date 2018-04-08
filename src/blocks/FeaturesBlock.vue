41344339271538cc31e640523f0a9434fb6e94aa
DenisShkain <denis.d.shakin@gmail.com>
2018-04-08 19:31:50 +0700

    added base structure for new block

--- src/blocks/FeaturesBlock.vue
+++ src/blocks/FeaturesBlock.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
-  section.feautures-block
-    h1 dkfjsdfjl
+  section.feautures
+    .features__row
+      block-component
 </template>
 
 <script>
+  import BlockComponent from './../components/landing-components/features/Block';
+
   export default {
-    name: "features-block"
+    name: "features-block",
+    components: {
+      BlockComponent
+    }
   }
 </script>
-
-<style scoped>
-  div {
-
-  }
-</style>
