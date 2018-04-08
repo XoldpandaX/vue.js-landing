89ca02b58c05e73a41f472883696cc71190b06f3
DenisShkain <denis.d.shakin@gmail.com>
2018-04-08 20:20:31 +0700

    removed excess code && add new avg to component

---
+++ src/components/landing-components/features/InfoBlock.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .info-block
+    .info-block__icon
+    .info-block__title
+    .info-block__text
+</template>
+
+<script>
+  export default {
+    name: "block"
+  }
+</script>
+
+<style scoped>
+
+</style>
