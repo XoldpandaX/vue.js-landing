30ea8aa8f026f1ae4dd5e70fd139fadbf556cbb5
Denis <Denis.Shakin@noveogroup.com>
2018-04-10 18:19:54 +0700

    added base styles for new block

--- src/blocks/AboutBlock.vue
+++ src/blocks/AboutBlock.vue
@@ -2,15 +2,18 @@
   section.about
     .container
       .about__row
-
+        info-block( v-for="i in 3",:key="123",
+        :selectedComponent="'StarIcon'", :background="'white'",
+        :title="'Design'", :description="'Curabitur eu nulla eget ligula laoreet auctor. Integer eu ultricies justo. Integer tortor nunc, tempor rutrum elementum vel, porttitor ac leo.'")
 </template>
 
 <script>
+  import InfoBlock from '../components/landing-components/features/InfoBlock';
+
   export default {
-    name: "AboutBlock"
+    name: "AboutBlock",
+    components: {
+      InfoBlock
+    }
   }
 </script>
-
-<style scoped>
-
-</style>
