de74a6639837d263aa2587be9e3ab452c9fc413b
Denis <Denis.Shakin@noveogroup.com>
2018-04-10 18:33:31 +0700

    added more styles for about block

--- src/blocks/AboutBlock.vue
+++ src/blocks/AboutBlock.vue
@@ -4,7 +4,9 @@
       .about__row
         info-block( v-for="i in 3",:key="123",
         :selectedComponent="'StarIcon'", :background="'white'",
-        :title="'Design'", :description="'Curabitur eu nulla eget ligula laoreet auctor. Integer eu ultricies justo. Integer tortor nunc, tempor rutrum elementum vel, porttitor ac leo.'")
+        :title="'Design'", :description="'Curabitur eu nulla eget ligula laoreet auctor. Integer eu ultricies justo.'")
+      .about__row.about__row--centered
+        button.btn.btn--primed.btn--important.btn--big Buy The Materializer Template
 </template>
 
 <script>
