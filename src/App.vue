5d1e22a981bd924ed38baa77e03a299f6700025e
Denis <Denis.Shakin@noveogroup.com>
2018-04-20 13:42:04 +0700

    added new block component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     slider-block
     features-block
     about-block
+    user-words-block
 </template>
 
 <script>
@@ -11,6 +12,7 @@
   import SliderBlock from './blocks/SliderBlock';
   import FeaturesBlock from './blocks/FeaturesBlock';
   import AboutBlock from './blocks/AboutBlock';
+  import UserWordsBlock from './blocks/UserWordsBlock';
   import { mapActions } from 'vuex';
 
   export default {
@@ -18,10 +20,8 @@
       ...mapActions('ui', ['closeAllMenu'])
     },
     components: {
-      HeaderBlock,
-      SliderBlock,
-      FeaturesBlock,
-      AboutBlock
+      HeaderBlock, SliderBlock, FeaturesBlock,
+      AboutBlock, UserWordsBlock
     }
   }
 </script>
---
+++ src/blocks/UserWordsBlock.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  section.user-words
+</template>
+
+<script>
+  export default {
+    name: "UserWordsBlock"
+  }
+</script>
+
+<style scoped>
+
+</style>
