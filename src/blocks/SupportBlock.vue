6db093274db571b8514208695436d6c66e8266cf
XoldpandaX <rembo-87@yandex.ru>
2018-05-05 10:15:56 +0700

    added new styles

--- src/blocks/SupportBlock.vue
+++ src/blocks/SupportBlock.vue
@@ -2,7 +2,9 @@
   section.support
     .container
       .support__inner
-        .support__description fsdff
+        .support__description
+          h2 Set your Support
+          p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.
         .support__media dfsdf
 
 </template>
