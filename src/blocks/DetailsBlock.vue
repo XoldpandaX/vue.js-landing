4ed5dc0ece9cc4d293f53a9fa1a2ecd042775f6c
XoldpandaX <rembo-87@yandex.ru>
2018-05-06 15:18:30 +0700

    added some styles

--- src/blocks/DetailsBlock.vue
+++ src/blocks/DetailsBlock.vue
@@ -4,7 +4,14 @@
       header
         h2 {{ title }}
         p {{ description }}
-      details-info-block(:selectedComponent="'EarthIcon'")
+      .details__block-line
+        details-info-block(:selectedComponent="'EarthIcon'")
+        details-info-block(:selectedComponent="'EarthIcon'")
+        details-info-block(:selectedComponent="'EarthIcon'")
+
+        details-info-block(:selectedComponent="'EarthIcon'")
+        details-info-block(:selectedComponent="'EarthIcon'")
+        details-info-block(:selectedComponent="'EarthIcon'")
 </template>
 
 <script>
